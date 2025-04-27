78863dc74f114bf4f801bdce913f956646f0b14f
Tyler Keller <tylerkeller@mines.edu>
2025-04-27 11:13:25 -0600

    results

--- lidar/run.sh
+++ lidar/run.sh
@@ -13,6 +13,18 @@ fi
 
 xhost +local:docker
 
+docker run -dit --rm \
+    --privileged \
+    --net=host \
+    --gpus all \
+    -e DISPLAY=$DISPLAY \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e LIBGL_ALWAYS_INDIRECT=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    -v "${PWD}/data":/data \
+    aloam
+
 container_id=$(docker run -dit --rm \
     --privileged \
     --net=host \
@@ -42,11 +54,15 @@ run_in_container "rosbag record /aft_mapped_to_init -O /data/output.bag __name:=
 run_in_container "rosbag play /data/00.bag"
 run_in_container "rosnode kill /aloam_bag"
 
+# d3p3ndency errors... weird
+run_in_container "python3.7 -m pip install tkinter"
+
 # restructuring .bag to .tum and running relevant evo commands
-run_in_container "cd /data && evo_traj bag output.bag /aft_mapped_to_init --save_as_tum"
-run_in_container "cd /data && evo_traj tum aft_mapped_to_init.tum sequence_00_GT.txt --ref=sequence_00_GT.txt --save_as_tum --plot_mode=xz --save_plot=traj_xz.pdf --no_warnings"
-run_in_container "cd /data && evo_ape tum sequence_00_GT.txt aft_mapped_to_init.tum --align --correct_scale --save_results=ape.json --save_plot=ape.pdf --no_warnings"
-run_in_container "cd /data && evo_rpe tum sequence_00_GT.txt aft_mapped_to_init.tum --ref=sequence_00_GT.txt --align --correct_scale --delta 1 --delta_unit s --all_pairs --save_results=rpe.json --plot --save_plot=rpe.pdf --no_warnings"
+mma cv
+evo_traj bag output.bag /aft_mapped_to_init --save_as_tum
+evo_traj tum aft_mapped_to_init.tum sequence_00_GT.txt --ref=sequence_00_GT.txt --save_as_tum --plot_mode=xz --save_plot=traj_xz.pdf --no_warnings
+evo_ape tum sequence_00_GT.txt aft_mapped_to_init.tum --align --correct_scale --save_results=ape.json --save_plot=ape.pdf --no_warnings
+evo_rpe tum sequence_00_GT.txt aft_mapped_to_init.tum --align --correct_scale --save_results=rpe.json --save_plot=rpe.pdf --no_warnings
 
 docker kill "${container_id}"
 xhost -local:docker
